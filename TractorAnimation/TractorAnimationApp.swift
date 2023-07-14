341c4b0a0b29532c8e755907a12a6de1d8962aa9
Ilnur Fattakhov <nurich1995@mail.ru>
2023-07-14 21:15:12 +0300

    Initial Commit

---
+++ TractorAnimation/TractorAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TractorAnimationApp.swift
+//  TractorAnimation
+//
+//  Created by Ilnur on 14.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TractorAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
